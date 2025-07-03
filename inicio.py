db43b46dbbafa2220da718796b2e888cbc60729e
Sabr2001 <santiagobonilla524@gmail.com>
2025-07-03 02:00:18 -0600

    Save Final
    Modificacion final de Estilo para Investigacion

--- inicio.py
+++ inicio.py
@@ -17,6 +17,12 @@ customtkinter.set_default_color_theme("green")  # Themes: "blue" (standard), "gr
 
 
 #================================= Variables y Datos Auxiliares =======================#
+imagenes_slider = [ 
+        {"ruta":r"'C:\Users\Santiago\Desktop\Ing. en Sistemas - UIA\3er CUATRIMESTRE\PROGRAMACION II\prograDos\programacionDos\assets\petmate2.png'"},
+        {"ruta":r"'C:\Users\Santiago\Desktop\Ing. en Sistemas - UIA\3er CUATRIMESTRE\PROGRAMACION II\prograDos\programacionDos\assets\dogs.png'"}
+    ]
+ejecucion_vista_inicio = False
+
 clientes = [
     {"nombre":"Santiago", "apellido":"Bonilla", "id":"1", "email":"sbr@hotmail.com", "mascota":"Rufus", "codigo":"ABC123"},
     {"nombre":"Rachell", "apellido":"McAdams","id":"2", "email":"rmcadams@hotmail.com", "mascota":"Fifi", "codigo":"DEF456"}
@@ -28,22 +34,36 @@ inventario = [
     {"id":"3", "nombre": "hueso de juguete", "categoria": "juguetes", "precio": "2.500", "cantidad": "12", "proveedor": "DogChow"},
     {"id":"45", "nombre": "collar", "categoria": "accesorios", "precio": "8.000", "cantidad": "7", "proveedor": "DogChow"},
 ]
-ejecucio_carga_inventario = False
-#################################INICIO METODOS Y FUNCIONES######################################################
+ejecucion_carga_inventario = False
 
-#
+servicios = [
+    {"id": "1", "tipo": "Peluquería", "precio": "₡10.000", "duracion": "45 min"},
+    {"id": "2", "tipo": "Consulta General", "precio": "₡15.000", "duracion": "30 min"},
+    {"id": "3", "tipo": "Venta de Accesorios", "precio": "₡Variable", "duracion": "N/A"},
+    {"id": "4", "tipo": "Venta de Medicinas", "precio": "₡Variable", "duracion": "N/A"},
+]
+ejecucion_carga_servicios = False
+#=================================Fin Variables y Datos Auxiliares =======================#
+
+#################################Inicio METODOS Y FUNCIONES######################################################
+
+#Define las pestana en la que nos ubicamos, eje central del codigo
 def pestana_por_defecto(name):
-    boton_inicio.configure(fg_color="gray" if name == "inicio" else "transparent")
-    boton_pestana1.configure(fg_color="gray" if name == "pestana1" else "transparent")
-    boton_pestana2.configure(fg_color="gray" if name == "pestana2" else "transparent")
-    boton_pestana3.configure(fg_color="gray" if name == "pestana3" else "transparent")
+    boton_inicio.configure(fg_color="gray" if name == "inicio" else "#3ba55d")
+    boton_pestana1.configure(fg_color="gray" if name == "pestana1" else "#3ba55d")
+    boton_pestana2.configure(fg_color="gray" if name == "pestana2" else "#3ba55d")
+    boton_pestana3.configure(fg_color="gray" if name == "pestana3" else "#3ba55d")
 
     if name == "inicio":
+        global ejecucion_vista_inicio
         pestana_inicio.grid(row=0, column=1, sticky="nsew")
         colocar_logo(pestana_inicio, imagen_tk)
         colocar_logo(pestana1, imagen_tk)
         colocar_logo(pestana2, imagen_tk)
         colocar_logo(pestana3, imagen_tk)
+        if ejecucion_vista_inicio == False:
+            vista_inicio(pestana_inicio)
+            ejecucion_vista_inicio = True
 
     else:
         pestana_inicio.grid_forget()
@@ -57,34 +77,117 @@ def pestana_por_defecto(name):
     else:
         pestana1.grid_forget()
     if name == "pestana2":
-        global ejecucio_carga_inventario
+        global ejecucion_carga_inventario
         pestana2.grid(row=0, column=1, sticky="nsew")
-        if ejecucio_carga_inventario == False:
+        if ejecucion_carga_inventario == False:
             index_inventario(pestana2)
-            ejecucio_carga_inventario = True
-        
-
-        
+            ejecucion_carga_inventario = True
     else:
         pestana2.grid_forget()
+
     if name == "pestana3":
+        global ejecucion_carga_servicios
         pestana3.grid(row=0, column=1, sticky="nsew")
+        if ejecucion_carga_servicios == False:
+            index_servicios(pestana3)
+            ejecucion_carga_servicios = True
     else:
         pestana3.grid_forget()
 
+#Redirecciones
 def redirigir_a_inicio():
     pestana_por_defecto("inicio")
-
+#Redirecciones
 def reidirigir_a_pestana1():
     pestana_por_defecto("pestana1")
-
+#Redirecciones
 def redirigir_a_pestana2():
     pestana_por_defecto("pestana2")
-
+#Redirecciones
 def redirigir_a_pestana3():
     pestana_por_defecto("pestana3")
 
+#Estilos para las vistas de cada pestaña
+def estilos():
+    config_estilo = ttk.Style() 
+
+    config_estilo.theme_use("default")
+
+    config_estilo.configure("Treeview",
+        background = "#2b2b2b",
+        foreground = "white",
+        fieldbackground = "#2b2b2b")
+    
+    config_estilo.configure("Treeview.Heading",
+        background = "#1F1F1F",
+        foreground = "white")
+    
+# Funcion para Recibir el cambio de tamaño de la ventana.
+def colocar_logo(pestana, imagen):
+                    
+    label_imagen = tk.Label(pestana, image=imagen, bg="#2b2b2b")
+    label_imagen.image = imagen
+    label_imagen.place(relx=1.0, y=5, anchor="ne")
+
+#funcion de la vista Inicio
+def vista_inicio(root):
+    
+    # Tab
+    tab = customtkinter.CTkTabview(master=root, width=460, height=220)
+    tab.grid(row=1, column=0, pady=(10, 20), padx=20)
+    tab.add("←")
+    tab.add("→")
+
+        # Centramos la imagen dentro del tab (←)
+    tab.tab("←").grid_rowconfigure(0, weight=1)
+    tab.tab("←").grid_columnconfigure(0, weight=1)
+
+    # Centramos la imagen dentro del tab (→)
+    tab.tab("→").grid_rowconfigure(0, weight=1)
+    tab.tab("→").grid_columnconfigure(0, weight=1)
+
+
+    # Logo
+    imagen_original = Image.open(r'C:\Users\Santiago\Desktop\Ing. en Sistemas - UIA\3er CUATRIMESTRE\PROGRAMACION II\prograDos\programacionDos\assets\petmate2.PNG')
+    imagen_redimensionada = imagen_original.resize((456, 200), Image.Resampling.LANCZOS)
+    imagen_tk = ImageTk.PhotoImage(imagen_redimensionada)
+    label_img1 = tk.Label(tab.tab("←"), image=imagen_tk, bg="#2b2b2b")
+    label_img1.image = imagen_tk
+    label_img1.grid(row=0, column=0, pady=10)
+
+    # Animales
+    imagen_original2 = Image.open(r'C:\Users\Santiago\Desktop\Ing. en Sistemas - UIA\3er CUATRIMESTRE\PROGRAMACION II\prograDos\programacionDos\assets\dogs.png')
+    imagen_redimensionada2 = imagen_original2.resize((456, 200), Image.Resampling.LANCZOS)
+    imagen_tk2 = ImageTk.PhotoImage(imagen_redimensionada2)
+    label_img2 = tk.Label(tab.tab("→"), image=imagen_tk2, bg="#2b2b2b")
+    label_img2.image = imagen_tk2
+    label_img2.grid(row=0, column=0, pady=10)
+
+    # === Espaciado entre carrusel y login ===
+    separador = customtkinter.CTkLabel(root, text="")  # Espacio vacío
+    separador.grid(row=2, column=0, pady=5)
+
+    # === Login Form ===
+    entry_usuario = customtkinter.CTkEntry(root, placeholder_text="Usuario", width=240)
+    entry_usuario.grid(row=3, column=0, pady=(10, 5))
+
+    entry_contrasena = customtkinter.CTkEntry(root, placeholder_text="Contraseña", show="*", width=240)
+    entry_contrasena.grid(row=4, column=0, pady=(5, 15))
+
+    # Contenedor de botones
+    contenedor_botones = customtkinter.CTkFrame(root, fg_color="transparent")
+    contenedor_botones.grid(row=5, column=0, pady=10)
+
+    btn_ingresar = customtkinter.CTkButton(contenedor_botones, text="Ingresar", width=100)
+    btn_ingresar.grid(row=0, column=0, padx=10)
+
+    btn_registrarse = customtkinter.CTkButton(contenedor_botones, text="Registrarse", width=100)
+    btn_registrarse.grid(row=0, column=1, padx=10)
+#funcion de la vista Clientes
 def index_clientes(root):
+
+    estilos()
+
     tree = ttk.Treeview(root, columns=("ID", "Nombre","Apellido", "Correo", "Mascota","Codigo"), show='headings')
     tree.heading("ID", text="ID")
     tree.heading("Nombre", text="Nombre")
@@ -98,16 +201,10 @@ def index_clientes(root):
 
     tree.pack(fill="both", expand=True)
 
-
-# Funcion para Recibir el cambio de tamaño de la ventana.
-def colocar_logo(pestana, imagen):
-                    
-    label_imagen = tk.Label(pestana, image=imagen, bg="#2b2b2b")
-    label_imagen.image = imagen
-    label_imagen.place(relx=1.0, y=5, anchor="ne")
-
-#funcion del inventario
+#funcion de la vista Inventario
 def index_inventario(root):
+    estilos()
+
     tree = ttk.Treeview(root, columns=("ID", "nombre", "categoria", "precio", "cantidad", "proveedor"), show='headings')
     tree.heading("ID", text="ID")
     tree.heading("nombre", text="nombre")
@@ -139,15 +236,10 @@ def index_inventario(root):
     
     tree.pack(fill="both", expand=True, padx=10, pady=10)
     
-#funcion del panel de Servicios
+#funcion de la vista Servicios
 def index_servicios(root):
-    servicios = [
-        {"id": "1", "tipo": "Peluquería", "precio": "₡10.000", "duracion": "45 min"},
-        {"id": "2", "tipo": "Consulta General", "precio": "₡15.000", "duracion": "30 min"},
-        {"id": "3", "tipo": "Venta de Accesorios", "precio": "₡Variable", "duracion": "N/A"},
-        {"id": "4", "tipo": "Venta de Medicinas", "precio": "₡Variable", "duracion": "N/A"},
-    ]
 
+    estilos()
     tree = ttk.Treeview(root, columns=("ID", "Servicio", "Precio", "Duración"), show="headings")
     tree.heading("ID", text="ID")
     tree.heading("Servicio", text="Servicio")
@@ -166,6 +258,7 @@ def index_servicios(root):
     scrollbar.pack(side="right", fill="y")
     tree.configure(yscrollcommand=scrollbar.set)
     tree.pack(fill="both", expand=True, padx=10, pady=10) 
+
 #################################FIN METODOS Y FUNCIONES########################################################
 
 ############################# Inicio Estructura y vista de la Ventana##############################################
